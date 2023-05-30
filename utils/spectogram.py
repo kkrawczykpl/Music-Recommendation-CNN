d286452df9838bacec298d411f44c088a05b3cfd
Krzysztof Krawczyk <krzysztof.krawczyk@ardentcode.com>
2023-05-30 14:43:52 +0200

    update function signature

--- utils/spectogram.py
+++ utils/spectogram.py
@@ -2,14 +2,15 @@
 
 """ 
     Create spectogram based on mp3 file
-"""
 
-def create_spectogram():
+    filepath -> path to mp3 file
+    output -> image in spectogram_images directory 
+"""
+def create_spectogram(filepath):
     return None
 
 """ 
     Split spectogram image into smaller parts
 """
-
 def split_spectogram():
     return None
